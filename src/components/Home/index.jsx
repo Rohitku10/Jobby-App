ef8d161cd615874fcbf88a177773e398d9689938
Rohit Kumar <rohitku101223@gmail.com>
2025-04-13 20:32:58 +0530

    header and initial commits

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,16 @@
+import Header from "../Header";
+
+const Home = () =>{
+    return(
+        <>
+        <Header/>
+        <div>
+            <h1>Find the job that fits your life</h1>
+            <p>Millions of people are searching for job daily</p>
+            <button>Finf jobs</button>
+        </div>
+        </>
+    )
+}
+
+export default Home;
