2c5616fcbffe6d73f095790a7b6dc52b84c7d4d6
Rohit Kumar <rohitku101223@gmail.com>
2025-04-13 20:48:37 +0530

    home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,28 @@
 import Header from "../Header";
+import { Link } from "react-router-dom";
+import "./index.css"
 
-const Home = () =>{
-    return(
-        <>
-        <Header/>
-        <div>
-            <h1>Find the job that fits your life</h1>
-            <p>Millions of people are searching for job daily</p>
-            <button>Finf jobs</button>
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <div className="home-content">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-description">
+            Millions of people are searching for jobs, salary information,
+            company reviews. Find the job that fits your abilities and
+            potential.
+          </p>
+          <Link to="/jobs">
+            <button type="button" className="shop-now-button">
+              Find Jobs
+            </button>
+          </Link>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Home;
