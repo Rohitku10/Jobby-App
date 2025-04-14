cd02393c0db5dd5874843629631b0f839999f2f1
Rohit Kumar <rohitku101223@gmail.com>
2025-04-14 23:52:56 +0530

    loginPage fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 
 import './App.css'
 import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+import  {Routes,Route} from "react-router-dom"
 
-const App = () => <Home/>
+const App = () =>{
+    return(
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<LoginForm/>}/>
+
+    </Routes>
+    )
+
+} 
 
 export default App
---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,72 @@
+import { Form } from "react-router-dom";
+import "./index.css";
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const renderUserNameField = () => {
+    return (
+      <>
+        <label className="user-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={onChangeUsername}
+        />
+      </>
+    );
+  };
+
+  const renderPasswordField = () => {
+    return (
+      <>
+        <label className="password-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={onChangePassword}
+        />
+      </>
+    );
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="login-form-container">
+      <form className="form-container" onSubmit={submitForm}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          className="website-logo"
+          alt="website logo"
+        />
+        <div className="input-container">{renderUserNameField()}</div>
+        <div className="input-container">{renderPasswordField()}</div>
+        <button className="login-button" type="submit">
+          Login
+        </button>
+        {/* {showSubmitError && <p className="error-message">*{errorMsg}</p>} */}
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
