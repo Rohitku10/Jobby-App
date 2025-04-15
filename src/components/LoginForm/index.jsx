0c05ca479c69ad0e817c3da58333a9b3aaa56d10
Rohit Kumar <rohitku101223@gmail.com>
2025-04-15 17:52:53 +0530

    cookie added

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,10 +1,23 @@
-import { Form } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import "./index.css";
 import { useState } from "react";
+import Cookies from "js-cookie";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const onsubmitSuccess = (jwt_token) => {
+    Cookies.set("jwt_token", jwt_token, { expires: 30 });
+    navigate("/")
+  };
+
+  const onsubmitFailure = (error_msg) =>{
+    setError(error_msg);
+  }
 
   const onChangeUsername = (event) => {
     setUsername(event.target.value);
@@ -48,23 +61,26 @@ const LoginForm = () => {
     );
   };
 
-  const submitForm = async(event) => {
+  const submitForm = async (event) => {
     event.preventDefault();
 
-    const userDetails = {username,password}
-    const loginApiUrl = 'https://apis.ccbp.in/login'
+    const userDetails = { username, password };
+    const loginApiUrl = "https://apis.ccbp.in/login";
     const options = {
-        method : "POST",
-        body : JSON.stringify(userDetails)
-    }
+      method: "POST",
+      body: JSON.stringify(userDetails),
+    };
 
-    const response = await fetch(loginApiUrl,options)
-    const responseData = response.json();
-    console.log(responseData);
-    if(response.ok == true){
-        console.log("login successfull")
+    const response = await fetch(loginApiUrl, options);
+    const data = await response.json();
+    console.log(data);
+    if (response.ok == true) {
+      console.log("login successfull");
+      console.log(data.jwt_token);
+      onsubmitSuccess(data.jwt_token);
+    } else {
+      onsubmitFailure(data.error_msg);
     }
-
   };
 
   return (
@@ -80,7 +96,7 @@ const LoginForm = () => {
         <button className="login-button" type="submit">
           Login
         </button>
-        {/* {showSubmitError && <p className="error-message">*{errorMsg}</p>} */}
+        {error && <p className="error-message">*{error}</p>}
       </form>
     </div>
   );
